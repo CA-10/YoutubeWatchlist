33b9a2098f69836e2ed1ca5056b352095d1348c0
CA <95572802+CA-10@users.noreply.github.com>
2025-06-24 23:14:36 +0100

    Created loose tables and code to load data from them.

--- YoutubeWatchlist.Library/Services/IDatabaseService.cs
+++ YoutubeWatchlist.Library/Services/IDatabaseService.cs
@@ -1,6 +1,8 @@
-﻿namespace YoutubeWatchlist.Library.Services;
+﻿using YoutubeWatchlist.Library.Models;
+
+namespace YoutubeWatchlist.Library.Services;
 
 public interface IDatabaseService
 {
-
+	public Task<(bool success, List<Video> videos)> GetVideosAsync();
 }
--- YoutubeWatchlist.Library/Services/SQLiteService.cs
+++ YoutubeWatchlist.Library/Services/SQLiteService.cs
@@ -1,11 +1,91 @@
 ﻿using Microsoft.Data.Sqlite;
+using YoutubeWatchlist.Library.Models;
 
 namespace YoutubeWatchlist.Library.Services;
 
 public class SQLiteService : IDatabaseService
 {
+	private string createStatements = """
+			CREATE TABLE IF NOT EXISTS videos (
+			NAME VARCHAR(255),
+			CATEGORY VARCHAR(255),
+			TIMESTAMP_SECONDS INT
+		);
+	""";
+
 	public SQLiteService()
 	{
-		
+
+	}
+
+	private bool CreateTablesIfNotExist()
+	{
+		bool success = true;
+
+		try
+		{
+			using (var connection = new SqliteConnection("Data Source=database.db3"))
+			{
+				connection.Open();
+
+				using (var command = new SqliteCommand(createStatements, connection))
+				{
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			success = false;
+		}
+
+		return success;
+	}
+
+	public async Task<(bool success, List<Video> videos)> GetVideosAsync()
+	{
+		bool success = true;
+		List<Video> videos = new();
+
+		string query = """
+			SELECT * FROM videos;
+		""";
+
+		try
+		{
+			using (var connection = new SqliteConnection("Data Source=database.db"))
+			{
+				using var command = new SqliteCommand(query, connection);
+
+				using (var reader = await command.ExecuteReaderAsync())
+				{
+					if (reader.HasRows)
+					{
+						while (await reader.ReadAsync())
+						{
+							string name = reader.GetString(reader.GetOrdinal("NAME"));
+							string categoryID = reader.GetString(reader.GetOrdinal("CATEGORY_ID"));
+							int timestampSeconds = reader.GetInt32(reader.GetOrdinal("TIMESTAMP_SECONDS"));
+
+							Video video = new Video()
+							{
+								Name = name,
+								CategoryID = categoryID,
+								TimestampSeconds = timestampSeconds
+							};
+
+							videos.Add(video);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			success = false;
+			videos = new();
+		}
+
+		return (success, videos);
 	}
 }
